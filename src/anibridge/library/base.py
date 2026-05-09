c99b6c682cbf0615133894d001da0081277a4747
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-05-09 11:39:03 +0000

    style: Use root logger

--- src/anibridge/library/base.py
+++ src/anibridge/library/base.py
@@ -293,7 +293,7 @@ class LibraryProvider(ABC):
             config (dict | None): Any configuration options that were detected with the
                 provider's namespace as a prefix.
         """
-        self.log = logger.getChild(f"library.{self.NAMESPACE}")
+        self.log = logger
         self.config: dict = config or {}
 
     async def initialize(self) -> None:
